9dd5138ff723e98e6d3603d9545217b018956d87
Tarif Halabi <tarif.halabi@gmail.com>
2017-04-02 22:09:24 -0400

    Cleanup imports.

--- StudentNotesService/src/test/java/com/kerneldc/education/studentNotesService/StudentNotesResourceTests.java
+++ StudentNotesService/src/test/java/com/kerneldc/education/studentNotesService/StudentNotesResourceTests.java
@@ -18,7 +18,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
