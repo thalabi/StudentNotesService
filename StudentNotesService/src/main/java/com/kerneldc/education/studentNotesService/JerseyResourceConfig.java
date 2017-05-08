916f0b7ae8cb622d813ba122e654a9bd440df5d9
Tarif Halabi <tarif.halabi@gmail.com>
2017-05-07 21:10:30 -0400

    Enable Jersey logging of HTTP requests and responses.

--- StudentNotesService/src/main/java/com/kerneldc/education/studentNotesService/JerseyResourceConfig.java
+++ StudentNotesService/src/main/java/com/kerneldc/education/studentNotesService/JerseyResourceConfig.java
@@ -1,5 +1,6 @@
 package com.kerneldc.education.studentNotesService;
 
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import com.kerneldc.education.studentNotesService.exception.ResponseSnsExceptionMapper;
@@ -11,11 +12,19 @@ import com.kerneldc.education.studentNotesService.util.JacksonObjectMapperProvid
 
 public class JerseyResourceConfig extends ResourceConfig {
 
+	private static final int JERSEY_LOGGING_MAX_ENTITY_SIZE = 2048;
 	public JerseyResourceConfig() {
 
 		// Create Jersey configuration with Jackson object mapper provider
 		super (JacksonObjectMapperProvider.class);
 
+		// Logging
+		register(new LoggingFeature(
+				java.util.logging.Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME),
+				java.util.logging.Level.INFO,
+				LoggingFeature.Verbosity.PAYLOAD_TEXT,
+				JERSEY_LOGGING_MAX_ENTITY_SIZE));
+		
 		// Specifying a package does not work due to Jersey classpath scanning limitations
 		// See: https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-1.4-Release-Notes#jersey-classpath-scanning-limitations
 		// Listing of Jersey resources is required as a workaround
