eb5e12ca4948629cea828d0b09a0bd145482a5f0
thalabi <thalabi@DT-THALABI.cppib.ca>
2017-11-27 14:10:48 -0500

    Cleanup unnecessary methods in security package.

--- StudentNotesService/src/main/java/com/kerneldc/education/studentNotesService/security/WebSecurityConfig.java
+++ StudentNotesService/src/main/java/com/kerneldc/education/studentNotesService/security/WebSecurityConfig.java
@@ -1,10 +1,7 @@
 package com.kerneldc.education.studentNotesService.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,10 +9,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.kerneldc.education.studentNotesService.security.config.AuthenticationFilter;
 import com.kerneldc.education.studentNotesService.security.config.CrossOriginResourceSharingFilter;
 import com.kerneldc.education.studentNotesService.security.config.RestAuthenticationEntryPoint;
-import com.kerneldc.education.studentNotesService.security.config.AuthenticationFilter;
-import com.kerneldc.education.studentNotesService.security.service.UserLoginService;
 
 @Configuration
 @EnableWebSecurity
@@ -26,14 +22,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthenticationFilter authenticationFilter;
     @Autowired
-    private UserLoginService userLoginService;
-    @Autowired
     private CrossOriginResourceSharingFilter crossOriginResourceSharingFilter;
     
-	public WebSecurityConfig() {
-
-	}
-	
 	@Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
@@ -52,16 +42,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             	UsernamePasswordAuthenticationFilter.class)
             ;
     }
-	
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       auth.userDetailsService(userLoginService);
-    }
-
-    @Override
-    @Bean // Enable autowiring of "AuthenticationManager" (e.g. LoginController)
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-       return super.authenticationManagerBean();
-    }
-
 }
